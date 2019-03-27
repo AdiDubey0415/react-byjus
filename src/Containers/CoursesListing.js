2266049dea0511cdcaea27a63bb484b1e9e20b60
Aditya  Dubey <amit.codemancers@weinvest.net>
2019-03-27 15:41:08 +0530

    reducers

---
+++ src/Containers/CoursesListing.js
@@ -0,0 +1,45 @@
+import React ,{ Component } from 'react';
+import CourseCard from '../Components/CourseCard';
+
+import { connect } from "react-redux";
+
+class CoursesList extends Component{
+    renderList(){
+        return this.props.courses.map( (course) => {
+            return <li key={course.title}>{course.title}</li>
+        })
+    }
+    render(){
+        return(
+            <div className="container">
+                <div className="row d-flex flex-row-reverse align-items-center">
+                    <div className="my-4">
+                        <span className="h6">Total Number of courses: </span>
+                        <span>12</span>
+                    </div>
+                </div>
+                <div className="row">
+                    <CourseCard />
+                    <CourseCard />
+                    <CourseCard />
+                    <CourseCard />
+                    <CourseCard />
+                    <CourseCard />
+                    <CourseCard />
+                    <CourseCard />
+                    <CourseCard />
+                    <span>{this.renderList()}</span>
+                </div>
+            </div>
+            
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+      courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps)(CoursesList)
